9585321484cc724e268f05dd5a0c0d1fac3ebe60
Benjamin DANDRE <dandre.ben@gmail.com>
2022-04-01 12:33:39 +0200

    home3 done

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'contact', to: 'pages#contact'
+  get 'project', to: 'pages#project'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles
   resources :episodes
