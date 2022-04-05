3c30672b727d566f01bd5987af9f29656814c32e
Benjamin DANDRE <dandre.ben@gmail.com>
2022-04-05 19:47:07 +0200

    home4

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
     before_action :set_article, only:[:show, :edit, :update, :destroy]
 
     def index
-        @acticles = Article.all
+        @articles = Article.all
     end
 
     def show
