8a1e55a61bf93f56a6c6be146113b844eb40b2c9
Benjamin DANDRE <dandre.ben@gmail.com>
2022-09-01 18:19:12 +0200

    done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+    def bgpic_placeholder(elem)
+        if elem.photo.attached?
+            cl_image_path elem.photo.key, height: 300, crop: :fill
+        else
+            'https://res.cloudinary.com/from-scratch/image/upload/v1661964287/jlpgp1trqoit0wps3psr.jpg'
+        end
+    end
+
+    def pic_placeholder(elem, height, width)
+        if elem.photo.attached?
+            cl_image_tag(elem.photo.key, height: height, width: width, crop: :fill)
+        else
+            image_tag("Ship-100.jpg", :alt =>"fusée", :style => "width: #{width}px; height: #{height}px")
+        end
+    end
+
 end
